14fea0126d0cd4e25d0e42bde1f1faf227c8a703
flexyzwork <flexyzwork@gmail.com>
2025-04-23 11:56:45 +0900

    [DOCKER] docker & compose

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ["@repo/db", "@repo/typescript-config", "@repo/eslint-config"],
+  output: "standalone",
   env: {
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
     UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
