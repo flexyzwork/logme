f93fc3b1445fe83bfd6bd1e586551e78e54137e3
flexyzwork <flexyzwork@gmail.com>
2025-04-25 19:22:50 +0900

    [FIX] sentry

---
+++ apps/web/webpack.config.js
@@ -0,0 +1,19 @@
+// webpack.config.js
+const { sentryWebpackPlugin } = require('@sentry/webpack-plugin')
+// for webpack 5.1 and webpack compatible environments
+// const { sentryWebpackPlugin } = require("@sentry/webpack-plugin/webpack5");
+
+module.exports = {
+  // ... other config above ...
+
+  devtool: 'source-map', // Source map generation must be turned on
+  plugins: [
+    sentryWebpackPlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+
+      // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
+}
