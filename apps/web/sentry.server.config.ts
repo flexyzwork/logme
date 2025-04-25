d8e132cf9e1feb3379f6cb146c81e36bd7366221
flexyzwork <flexyzwork@gmail.com>
2025-04-25 19:44:11 +0900

    [FIX] sentry

--- apps/web/sentry.server.config.ts
+++ apps/web/sentry.server.config.ts
@@ -7,6 +7,9 @@ import * as Sentry from "@sentry/nextjs";
 Sentry.init({
   dsn: "https://66531cbf6a4631f72da3b3738498e48e@o4509212091940864.ingest.de.sentry.io/4509212092399696",
 
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 });
