4b06e40e4d8d646761ddfcd8bfba77b9b77c574e
flexyzwork <flexyzwork@gmail.com>
2025-04-27 13:42:50 +0900

    [FIX] test

--- apps/web/src/lib/logger/serverLogger.ts
+++ apps/web/src/lib/logger/serverLogger.ts
@@ -1,10 +1,10 @@
-import { ServerBetterStackStrategy } from './strategies/better-stack/ServerBetterStackStrategy';
+// import { ServerBetterStackStrategy } from './strategies/better-stack/ServerBetterStackStrategy';
 // import { ServerSlackStrategy } from './strategies/slack/ServerSlackStrategy';
-import { ServerSentryStrategy } from './strategies/sentry/ServerSentryStrategy';
+// import { ServerSentryStrategy } from './strategies/sentry/ServerSentryStrategy';
 import { Logger } from '@/lib/logger/Logger';
 
 export const serverLogger = new Logger([
-  new ServerBetterStackStrategy(),
+  // new ServerBetterStackStrategy(),
   // new ServerSlackStrategy(),
-  new ServerSentryStrategy(),
+  // new ServerSentryStrategy(),
 ]);
