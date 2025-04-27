f94a0c2f0dde906982fb9c7606b85b604dc9fac1
flexyzwork <flexyzwork@gmail.com>
2025-04-27 12:24:52 +0900

    [FIX] bs - test

--- apps/web/src/lib/logger/strategies/better-stack/ServerBetterStackStrategy.ts
+++ apps/web/src/lib/logger/strategies/better-stack/ServerBetterStackStrategy.ts
@@ -1,42 +1,42 @@
-// /* eslint-disable @typescript-eslint/no-explicit-any */
-// import { Logger, withBetterStack, BetterStackRequest } from '@logtail/next'
-// import { LoggerStrategy, LogLevel } from '@/lib/logger/strategies/LoggerStrategy'
-
-// export class ServerBetterStackStrategy implements LoggerStrategy {
-//   private logger: Logger
-
-//   constructor() {
-//     this.logger = new Logger()
-//   }
-
-//   async log(level: LogLevel, message: string, meta?: Record<string, any>) {
-//     this.logger[level](message, meta)
-//     await this.logger.flush()
-//   }
-
-//   withLogging(handler: (req: BetterStackRequest) => any) {
-//     return withBetterStack(handler)
-//   }
-// }
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import logger from '@/lib/logger'
+import { Logger, withBetterStack, BetterStackRequest } from '@logtail/next'
 import { LoggerStrategy, LogLevel } from '@/lib/logger/strategies/LoggerStrategy'
 
 export class ServerBetterStackStrategy implements LoggerStrategy {
-  async log(level: LogLevel, message: string, meta?: Record<string, any>) {
-    try {
-      const baseUrl = process.env.NEXT_PUBLIC_API_URL
-      if (!baseUrl) {
-        throw new Error('NEXT_PUBLIC_API_URL is not defined')
-      }
+  private logger: Logger
 
-      await fetch(`${baseUrl}/api/internal/log`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ level, message, meta }),
-      })
-    } catch ( error ) {
-      logger.log('error', '🔴 Failed to send log from client:', { error })
-    }
+  constructor() {
+    this.logger = new Logger()
+  }
+
+  async log(level: LogLevel, message: string, meta?: Record<string, any>) {
+    this.logger[level](message, meta)
+    await this.logger.flush()
+  }
+
+  withLogging(handler: (req: BetterStackRequest) => any) {
+    return withBetterStack(handler)
   }
 }
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// import logger from '@/lib/logger'
+// import { LoggerStrategy, LogLevel } from '@/lib/logger/strategies/LoggerStrategy'
+
+// export class ServerBetterStackStrategy implements LoggerStrategy {
+//   async log(level: LogLevel, message: string, meta?: Record<string, any>) {
+//     try {
+//       const baseUrl = process.env.NEXT_PUBLIC_API_URL
+//       if (!baseUrl) {
+//         throw new Error('NEXT_PUBLIC_API_URL is not defined')
+//       }
+
+//       await fetch(`${baseUrl}/api/internal/log`, {
+//         method: 'POST',
+//         headers: { 'Content-Type': 'application/json' },
+//         body: JSON.stringify({ level, message, meta }),
+//       })
+//     } catch ( error ) {
+//       logger.log('error', '🔴 Failed to send log from client:', { error })
+//     }
+//   }
+// }
