87541236874092454abd3864222d953f61665f47
flexyzwork <flexyzwork@gmail.com>
2025-04-27 17:43:38 +0900

    \[FIX\] bs-test

--- apps/web/src/lib/logger/index.ts
+++ apps/web/src/lib/logger/index.ts
@@ -1,7 +1,10 @@
 import { serverLogger } from './serverLogger'
 import { clientLogger } from './clientLogger'
 
-const isServer = typeof window === 'undefined' && process.env.NEXT_PUBLIC_BASE_URL === undefined
+const isServer =
+  typeof window === 'undefined' &&
+  process.env.NEXT_PUBLIC_BASE_URL === undefined &&
+  process.env.NEXT_RUNTIME !== 'edge'
 
 export const logger = isServer ? serverLogger : clientLogger
 export default logger
