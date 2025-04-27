f34901646985db126ce629f21390cdd05086d9f0
flexyzwork <flexyzwork@gmail.com>
2025-04-27 19:04:27 +0900

    [LOG] bs-test

--- apps/web/src/lib/logger/index.ts
+++ apps/web/src/lib/logger/index.ts
@@ -1,5 +1,7 @@
-import { serverLogger } from './serverLogger'
+// import { serverLogger } from './serverLogger'
 import { clientLogger } from './clientLogger'
 
-export const logger = typeof window === 'undefined' ? serverLogger : clientLogger
+// export const logger = typeof window === 'undefined' ? serverLogger : clientLogger
+
+export const logger = clientLogger
 export default logger
